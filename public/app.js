21502b1f236fdd380285a035190aa79663878aad
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-08-15 17:02:06 -0400

    fixed chessboard.js piece images

--- public/app.js
+++ public/app.js
@@ -1,10 +1,18 @@
 console.log("app.js says 'En Garde'!");
 
-const $testDisplay = $("#testDisplay")
-const testBoard = document.querySelector("#testBoard")
+// const $testDisplay = $("#testDisplay")
+// const testBoard = document.querySelector("#testBoard")
 
-console.log($testDisplay, testBoard)
+// console.log($testDisplay, testBoard)
 
-$testDisplay.append(Chessboard(testBoard, "start"))
+// $testDisplay.append(Chessboard(testBoard, "start"))
 
+const $gameContainers = $(".gameContainer");
+// console.log($gameContainers.children());
 
+$gameContainers.children().each((idx, div) => {
+  console.log($(div).children()[0].innerText);
+  const fen = $(div).children()[0].innerText
+  Chessboard(div, fen)
+  // $(div).child()
+});
