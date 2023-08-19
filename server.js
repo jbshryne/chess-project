f538a95266178433910ff1cf8e28aaade4b279a0
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-08-19 14:55:35 -0400

    add node version to package.json

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require("express");
 require("dotenv").config();
-const PORT = process.env.PORT || 6464
-const NODE_VERSION = "14.17.0"
+const PORT = process.env.PORT || 6464;
 const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
 const authRoutes = require("./controllers/authController");
 const gameRoutes = require("./controllers/gameController");
 const app = express();
-
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
 app.use(session({ secret: "PompomElves", cookie: { maxAge: 720000 } }));
 
 app.get("/dburl", (req, res) => {
-    res.send("Connection URL: " + process.env.DATABASE_URL)
-})
+  res.send("Connection URL: " + process.env.DATABASE_URL);
+});
 
 app.use(authRoutes);
 
